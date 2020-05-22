5bf5a0ee4b50a38a1858b2918c4e28972f5d3681
shani <git@gmail.com>
2020-05-22 11:10:23 +0000

    webhooks project

---
+++ frozen/src/main/java/com/frozen/App.java
@@ -0,0 +1,13 @@
+package com.frozen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
